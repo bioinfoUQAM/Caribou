64728b8c7989df11c11e0b7c1c05f481151a7152
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2021-10-26 15:53:39 -0400

    seqfile

--- metagenomics_ML/data/build_data.py
+++ metagenomics_ML/data/build_data.py
@@ -14,10 +14,12 @@ __author__ = "nicolas"
 def build_load_save_data(file, hostfile, prefix, dataset, k=4, full_kmers=False, low_var_threshold=None):
 
     # Generate the names of files
-    Xy_file = prefix + "_K{}_Xy_genome_{}_data.h5f".format(k,dataset)
-    data_file = prefix + "_K{}_Xy_genome_{}_data.npz".format(k,dataset)
-    Xy_file_host = prefix + "_K{}_Xy_genome_{}_host_data.h5f".format(k,dataset)
-    data_file_host = prefix + "_K{}_Xy_genome_{}_host_data.npz".format(k,dataset)
+    Xy_file = "{}_K{}_Xy_genome_{}_data.h5f".format(prefix,k,dataset)
+    data_file = "{}_K{}_Xy_genome_{}_data.npz".format(prefix,k,dataset)
+    Xy_file_host = "{}_K{}_Xy_genome_{}_host_data.h5f".format(prefix,k,dataset)
+    data_file_host = "{}_K{}_Xy_genome_{}_host_data.npz".format(prefix,k,dataset)
+    seqfile = "{}_seqdata_db_{}.txt".format(prefix, dataset)
+    seqfile_host = "{}_seqdata_host_{}.txt".format(prefix, dataset)
 
     # Load file if already exists
     if os.path.isfile(data_file) and os.path.isfile(data_file_host) and isinstance(hostfile, tuple):
@@ -31,20 +33,20 @@ def build_load_save_data(file, hostfile, prefix, dataset, k=4, full_kmers=False,
         if isinstance(file, tuple):
 # To accelerate testing with bigger database
 # Possibility of adding option for user
-            if not os.path.isfile("/home/nicolas/github/metagenomics_ML/data/output/mock/seq_data.txt"):
+            if not os.path.isfile(seqfile):
                 print("seq_data")
                 seq_data = SeqCollection((list(file)[0], list(file)[1]))
-                with open("/home/nicolas/github/metagenomics_ML/data/output/mock/seq_data.txt", "wb") as handle:
+                with open(seqfile, "wb") as handle:
                     pickle.dump(seq_data, handle)
                 if isinstance(hostfile, tuple):
                     seq_data_host = SeqCollection((list(hostfile)[0], list(hostfile)[1]))
-                    with open("/home/nicolas/github/metagenomics_ML/data/output/mock/seq_data_host.txt", "wb") as handle:
+                    with open(seqfile_host, "wb") as handle:
                         pickle.dump(seq_data_host, handle)
             else:
-                with open("/home/nicolas/github/metagenomics_ML/data/output/mock/seq_data.txt", "rb") as handle:
+                with open(seqfile, "rb") as handle:
                     seq_data = pickle.load(handle)
                 if isinstance(hostfile, tuple):
-                    with open("/home/nicolas/github/metagenomics_ML/data/output/mock/seq_data_host.txt", "rb") as handle:
+                    with open(seqfile_host, "rb") as handle:
                         seq_data_host = pickle.load(handle)
 
             # Build Xy_data to drive
