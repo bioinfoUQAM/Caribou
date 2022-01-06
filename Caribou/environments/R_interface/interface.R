29a7a5c056d06b4fb99efe9d34512352efe74e08
Nicolas de Montigny <bf491015@ens.uqam.ca>
2022-01-06 01:04:28 -0500

    R

--- Caribou/environments/R_interface/interface.R
+++ Caribou/environments/R_interface/interface.R
@@ -5,16 +5,19 @@ library(reticulate)
   caribou = import("Caribou", delay_load = TRUE)
 }
 
+caribou = function(config_file):
+  caribou$Caribou$caribou(r_to_py(c("Caribou.py",config_file), convert = T))
+
 build_data = function(file, hostfile, prefix, dataset, kmers_list=None, k=4, full_kmers=False, low_var_threshold=None){
-  caribou$data$build_data$build_load_save_data(file, hostfile, prefix, dataset, kmers_list, k, full_kmers, low_var_threshold)
+  return(py_to_r(caribou$data$build_data$build_load_save_data(file, hostfile, prefix, dataset, kmers_list, k, full_kmers, low_var_threshold)))
 }
 
 bacteria_extraction = function(metagenome_k_mers, database_k_mers, k, outdirs, dataset, classifier = "deeplstm", batch_size = 32, verbose = 1, cv = 1, saving_host = 1, saving_unclassified = 1, n_jobs = 1){
-  caribou$models$bacteria_extraction$bacteria_extraction(metagenome_k_mers, database_k_mers, k, outdirs, dataset, classifier, batch_size, verbose, cv, saving_host, saving_unclassified, n_jobs)
+  return(py_to_r(caribou$models$bacteria_extraction$bacteria_extraction(metagenome_k_mers, database_k_mers, k, outdirs, dataset, classifier, batch_size, verbose, cv, saving_host, saving_unclassified, n_jobs)))
 }
 
 classification = function(classified_data, database_k_mers, k, outdirs, dataset, classifier = "lstm_attention", batch_size = 32, threshold = 0.8, verbose = 1, cv = 1, n_jobs = 1){
-  caribou$models$classification$bacteria_classification(classified_data, database_k_mers, k, outdirs, dataset, classifier, batch_size, threshold, verbose, cv, n_jobs)
+  return(py_to_r(caribou$models$classification$bacteria_classification(classified_data, database_k_mers, k, outdirs, dataset, classifier, batch_size, threshold, verbose, cv, n_jobs)))
 }
 
 outputs = function(database_kmers, results_dir, k, classifier, dataset, host, classified_data, seq_file, abundance_stats = True, kronagram = True, full_report = True){
