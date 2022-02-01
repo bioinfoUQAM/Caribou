9ae94c20f84f8bbc25a1e8e4c7b87ae935dab1cb
Nicolas de Montigny <bf491015@ens.uqam.ca>
2022-02-01 13:01:51 -0500

    mem alloc to default

--- Caribou/data/kmer_collections.py
+++ Caribou/data/kmer_collections.py
@@ -107,7 +107,7 @@ def construct_data(dict_data, Xy_file):
 
 def compute_seen_kmers_of_sequence(dict_data, kmc_path, k, dir_path, ind, file):
     # Count k-mers with KMC
-    cmd_count = "{}/kmc -k{} -fm -cs1000000000 -t48 -hp -sm -m1024 {} {}/{} {}".format(kmc_path, k, file, dir_path, ind, dir_path)
+    cmd_count = "{}/kmc -k{} -fm -cs1000000000 -t68 -hp -sm {} {}/{} {}".format(kmc_path, k, file, dir_path, ind, dir_path)
     run(cmd_count, shell = True)#, capture_output=True)
     # Transform k-mers db with KMC
     cmd_transform = "{}/kmc_tools transform {}/{} dump {}/{}.txt".format(kmc_path, dir_path, ind, dir_path, ind)
@@ -117,12 +117,12 @@ def compute_seen_kmers_of_sequence(dict_data, kmc_path, k, dir_path, ind, file):
     # Save to Xyfile
     for row in profile:
         dict_data[row[0]][ind] = int(row[1])
-    print(os.listdir(dir_path))
+
     return dict_data
 
 def compute_given_kmers_of_sequence(dict_data, kmers_list, kmc_path, k, dir_path, ind, file):
     # Count k-mers with KMC
-    cmd_count = "{}/kmc -k{} -fm -cs1000000000 -t48 -hp -sm -m1024 {} {}/{} {}".format(kmc_path, k, file, dir_path, ind, dir_path)
+    cmd_count = "{}/kmc -k{} -fm -cs1000000000 -t68 -hp -sm {} {}/{} {}".format(kmc_path, k, file, dir_path, ind, dir_path)
     run(cmd_count, shell = True, capture_output=True)
     # Transform k-mers db with KMC
     cmd_transform = "{}/kmc_tools transform {}/{} dump {}/{}.txt".format(kmc_path, dir_path, ind, dir_path, ind)
@@ -160,8 +160,8 @@ def compute_kmers(seq_data, method, dict_data, kmers_list, k, dir_path, faSplit,
         t_start = time.time()
         loky(file_list, method, dict_data, kmers_list, kmc_path, k, dir_path)
         print("Joblib loky : {:.3f}s".format(time.time() - t_start))
-        #rmtree(dir_path)
-        #os.mkdir(dir_path)
+        rmtree(dir_path)
+        os.mkdir(dir_path)
         dict_data = {}
     except Error as e:
         print("Joblib loky did not work")
