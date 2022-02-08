f6d1945f31a50a38aabcfcedbc5cddcd21043801
Nicolas de Montigny <bf491015@ens.uqam.ca>
2022-02-08 16:00:35 -0500

    loky test

--- Caribou/data/kmer_collections.py
+++ Caribou/data/kmer_collections.py
@@ -204,14 +204,12 @@ def dask_client(file_list, method, dict_data, kmers_list, kmc_path, k, dir_path)
 
 def joblib_loky(file_list, method, dict_data, kmers_list, kmc_path, k, dir_path):
     if method == 'seen':
-        with parallel_backend('loky'):
-            results = Parallel(n_jobs = -1, prefer = 'processes', verbose = 100)(
-            delayed(compute_seen_kmers_of_sequence)
-            (dict_data, kmc_path, k, dir_path, i, file) for i, file in enumerate(file_list))
+        results = Parallel(n_jobs = -1, prefer = 'processes', verbose = 100)(
+        delayed(compute_seen_kmers_of_sequence)
+        (dict_data, kmc_path, k, dir_path, i, file) for i, file in enumerate(file_list))
     elif method == 'given':
-        with parallel_backend('loky'):
-            results = Parallel(n_jobs = -1, prefer = 'processes', verbose = 100)(
-            delayed(compute_given_kmers_of_sequence)
-            (dict_data, kmers_list, kmc_path, k, dir_path, i, file) for i, file in enumerate(file_list))
+        results = Parallel(n_jobs = -1, prefer = 'processes', verbose = 100)(
+        delayed(compute_given_kmers_of_sequence)
+        (dict_data, kmers_list, kmc_path, k, dir_path, i, file) for i, file in enumerate(file_list))
 
     return results[0]
