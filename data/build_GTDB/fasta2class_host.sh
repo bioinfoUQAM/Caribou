e7d4b0badf0e79da219834dc18f7ce8d5b99567c
Nicolas de Montigny <bf491015@ens.uqam.ca>
2022-03-03 13:48:57 -0500

    change opt s -> g

--- data/build_GTDB/fasta2class_host.sh
+++ data/build_GTDB/fasta2class_host.sh
@@ -3,14 +3,14 @@
 __author__="Nicolas de Montigny"
 
 HELP=0
-while getopts d:i:s:o:h option; do
+while getopts d:i:g:o:h option; do
   case "${option}" in
     d) DIR=${OPTARG};;
     directory) DIR=${OPTARG};;
     i) FASTA_LIST=${OPTARG};;
     input) FASTA_LIST=${OPTARG};;
-    s) SPECIES=${OPTARG};;
-    species) SPECIES=${OPTARG};;
+    g) SPECIES=${OPTARG};;
+    genus) SPECIES=${OPTARG};;
     o) OUTDIR=${OPTARG};;
     output) OUTDIR=${OPTARG};;
     h) HELP=1;;
@@ -28,7 +28,7 @@ then
 
   -d --directory a directory containing all host fasta files
   -i --input a tsv/csv file containing path and names to all fasta files to extract ids from
-  -s --species name of the host species
+  -g --genus name of the host species
   -o --output Path to output directory
   -h --help Show this help message
   """
