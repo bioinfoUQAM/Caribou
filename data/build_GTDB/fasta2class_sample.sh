964cf7b1ff90af4a16d8b7d24b30f252f3ec9f3d
Nicolas de Montigny <bf491015@ens.uqam.ca>
2022-03-20 13:16:33 -0400

    easier usage of fasta2class_sample.sh

--- data/build_GTDB/fasta2class_sample.sh
+++ data/build_GTDB/fasta2class_sample.sh
@@ -9,8 +9,8 @@ while getopts "d:i:c:o:h" option; do
     input) FASTA=${OPTARG};;
     c) CLASSES_IN=${OPTARG};;
     classes) CLASSES_IN=${OPTARG};;
-    o) OUTDIR=${OPTARG};;
-    output) OUTDIR=${OPTARG};;
+    o) OUTFILE=${OPTARG};;
+    output) OUTFILE=${OPTARG};;
     h) HELP=1;;
     help) HELP=1;;
   esac
@@ -25,14 +25,13 @@ if [ $HELP -eq 1 ]; then
 
   -i --input A fasta file containing a subset of the database
   -c --classes A csv containing all classes of the database
-  -o --output Path to output files
+  -o --output File where the subset classes will be written
   -h --help Show this help message
   """
   exit 0
 fi
 
-cls_file=$OUTDIR/class_subset.csv
-echo "id","species","genus","family","order","class","phylum","domain" >> $cls_file
+echo "id","species","genus","family","order","class","phylum","domain" >> $OUTFILE
 
 if [[ $FASTA == *.gz ]]; then
   list_ids=$(zcat $FASTA | grep -o -E "^>\w+" | tr -d ">")
@@ -41,5 +40,5 @@ else
 fi
 
 for id in $list_ids; do
-  cat $CLASSES_IN | grep $id >> $cls_file
+  cat $CLASSES_IN | grep $id >> $OUTFILE
 done
