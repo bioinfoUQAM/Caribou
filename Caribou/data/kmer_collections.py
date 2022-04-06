0118afab02cf9f1a57281bc816ae65f881dc23bb
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2022-04-06 15:31:03 -0400

    wait ddf

--- Caribou/data/kmer_collections.py
+++ Caribou/data/kmer_collections.py
@@ -238,7 +238,7 @@ def construct_data_GPU(Xy_file, list_id_file):
     except:
         pass
 
-    wait(df)
+    wait(ddf)
     return save_kmers_profile_GPU(ddf, Xy_file, tmp = False)
 
 def save_kmers_profile_CPU(df, Xy_file, tmp = True):
