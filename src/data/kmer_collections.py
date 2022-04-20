a0abcbc712df00d294e44f86207ed41ed0f7ef8f
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2022-04-20 11:44:40 -0400

    remove try/except clause in kmers extract

--- src/data/kmer_collections.py
+++ src/data/kmer_collections.py
@@ -125,24 +125,6 @@ def compute_seen_kmers_of_sequence(kmc_path, k, dir_path, ind, file):
         # Make tmp folder per sequence
         tmp_folder = os.path.join(dir_path,"tmp_{}".format(ind))
         id = os.path.splitext(os.path.basename(file))[0]
-        try:
-            os.mkdir(tmp_folder)
-            # Count k-mers with KMC
-            cmd_count = os.path.join(kmc_path,"kmc -k{} -fm -ci1 -cs1000000000 -m10 -hp {} {} {}".format(k, file, os.path.join(tmp_folder, str(ind)), tmp_folder))
-            run(cmd_count, shell = True, capture_output=True)
-            # Transform k-mers db with KMC
-            cmd_transform = os.path.join(kmc_path,"kmc_tools transform {} dump {}".format(os.path.join(tmp_folder, str(ind)), os.path.join(dir_path, "{}.txt".format(ind))))
-            run(cmd_transform, shell = True, capture_output=True)
-        except:
-            pass
-
-        return id, os.path.join(dir_path,"{}.txt".format(ind))
-
-def compute_given_kmers_of_sequence(kmers_list, kmc_path, k, dir_path, ind, file):
-    # Make tmp folder per sequence
-    tmp_folder = os.path.join(dir_path,"tmp_{}".format(ind))
-    id = os.path.splitext(os.path.basename(file))[0]
-    try:
         os.mkdir(tmp_folder)
         # Count k-mers with KMC
         cmd_count = os.path.join(kmc_path,"kmc -k{} -fm -ci1 -cs1000000000 -m10 -hp {} {} {}".format(k, file, os.path.join(tmp_folder, str(ind)), tmp_folder))
@@ -150,8 +132,20 @@ def compute_given_kmers_of_sequence(kmers_list, kmc_path, k, dir_path, ind, file
         # Transform k-mers db with KMC
         cmd_transform = os.path.join(kmc_path,"kmc_tools transform {} dump {}".format(os.path.join(tmp_folder, str(ind)), os.path.join(dir_path, "{}.txt".format(ind))))
         run(cmd_transform, shell = True, capture_output=True)
-    except:
-        pass
+
+        return id, os.path.join(dir_path,"{}.txt".format(ind))
+
+def compute_given_kmers_of_sequence(kmers_list, kmc_path, k, dir_path, ind, file):
+    # Make tmp folder per sequence
+    tmp_folder = os.path.join(dir_path,"tmp_{}".format(ind))
+    id = os.path.splitext(os.path.basename(file))[0]
+    os.mkdir(tmp_folder)
+    # Count k-mers with KMC
+    cmd_count = os.path.join(kmc_path,"kmc -k{} -fm -ci1 -cs1000000000 -m10 -hp {} {} {}".format(k, file, os.path.join(tmp_folder, str(ind)), tmp_folder))
+    run(cmd_count, shell = True, capture_output=True)
+    # Transform k-mers db with KMC
+    cmd_transform = os.path.join(kmc_path,"kmc_tools transform {} dump {}".format(os.path.join(tmp_folder, str(ind)), os.path.join(dir_path, "{}.txt".format(ind))))
+    run(cmd_transform, shell = True, capture_output=True)
 
     try:
         profile = pd.read_table(os.path.join(dir_path,"{}.txt".format(ind)), names = [id], index_col = 0, dtype = object).T
