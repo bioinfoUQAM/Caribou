71865a90bbcd38bd3ab25a68a691fc705bbcaaa6
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2022-04-27 15:39:07 -0400

    prints for test

--- src/data/kmer_collections.py
+++ src/data/kmer_collections.py
@@ -103,15 +103,17 @@ def construct_data(Xy_file, dir_path, list_id_file, kmers_list):
 
     # Fill NAs with 0
     df = df.fillna(0)
+    print(df)
     # Get row sums
     df['sum'] = df.func.sum_row_wise(*[df[k] for k in colnames])
     # Filter dataframe to remove rows filled with 0
     df = df[df['sum'] > 0]
+    print(df)
     # Extract k-mers list
     kmers_list = list(df.kmers.values)
     # Convert to numpy array to transpose and reconvert to vaex df
     df = np.array(df.to_arrays(column_names  = colnames, array_type = 'numpy'), dtype = np.int32)
-    print(df)
+    print(df.shape)
     save_kmers_profile(df, Xy_file, tmp = False)
 
     return ids, kmers_list
