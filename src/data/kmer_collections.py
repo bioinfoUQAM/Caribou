cfa7f801571a2b5b1de4df9a1b2058a9ce15f35f
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2022-05-23 13:06:22 -0400

    kmers merged df only saved at end not while reading csv files

--- src/data/kmer_collections.py
+++ src/data/kmer_collections.py
@@ -64,7 +64,7 @@ def kmers_collection(seq_data, Xy_file, length, k, dataset, method = 'seen', kme
 
 def construct_data(Xy_file, dir_path):
 
-    df = vaex.open(os.path.join(dir_path,"*.csv"), convert = Xy_file)
+    df = vaex.open(os.path.join(dir_path,"*.csv"))
     colnames = list(df.columns)
     colnames.remove('id')
     # Fill NAs with 0
