2c971d8ffd13618738076a9774e27aa9f1572bb7
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2022-09-20 17:43:01 -0400

    reads_sim better way of finding ids

--- src/models/reads_simulation.py
+++ src/models/reads_simulation.py
@@ -111,23 +111,25 @@ class readsSimulation():
     def _fastq2fasta(self):
         with gzip.open(self._R1_fastq, "rt") as handle_R1, gzip.open(self._R2_fastq, "rt") as handle_R2, gzip.open(self._fasta_out, "at") as handle_out:
             for record_R1, record_R2 in zip(SeqIO.parse(handle_R1, 'fastq'), SeqIO.parse(handle_R2, 'fastq')):
-                record_R1.id = record_R1.id.replace('/','--')
-                record_R2.id = record_R2.id.replace('/','--')
+                record_R1.id = record_R1.id.replace('/','_')
+                record_R2.id = record_R2.id.replace('/','_')
                 SeqIO.write(record_R1, handle_out, 'fasta')
                 SeqIO.write(record_R2, handle_out, 'fasta')
 
     def _write_cls_file(self):
         with gzip.open(self._fasta_out, 'rt') as handle:
             reads_ids = [record.id for record in SeqIO.parse(handle, 'fasta')]
-        reads_crop = [id.split('--')[0] for id in reads_ids]
+        reads_crop = list(pd.read_csv(self._cls_in['id']))
         print(reads_crop)
-        reads_df = pd.DataFrame({'real_id' : reads_ids, 'id': reads_crop})
+        reads_df = pd.DataFrame({'reads_id' : reads_ids, 'id': np.empty(len(reads_ids), dtype=object)})
+        for id in reads_crop:
+            reads_df.loc[reads_df['reads_id'].str.contains(id),'id'] = id
         print(reads_df)
         cls_out = reads_df.join(self._cls_in.set_index('id'), on = 'id')
         print(cls_out)
         cls_out = cls_out.drop('id', axis = 1)
         print(cls_out)
-        cls_out = cls_out.rename(columns = {'real_id':'id'})
+        cls_out = cls_out.rename(columns = {'reads_id':'id'})
         print(cls_out)
         cls_out.to_csv(self._cls_out, index = False)
         print(cls_out)
